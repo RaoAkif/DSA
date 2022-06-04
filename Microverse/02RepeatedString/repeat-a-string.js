73f1a70ef79c014c27dd6009f956bea47183b734
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2022-06-04 16:06:11 +0500

    Update repeat-a-string.js

--- Microverse/02RepeatedString/repeat-a-string.js
+++ Microverse/02RepeatedString/repeat-a-string.js
@@ -1,11 +1,4 @@
 function repeatStringNumTimes (str, num) {
-  let repeatedString = '';
-  if (num <= 0) return '';
-
-  for (let i=1; i<=num; i++) {
-    repeatedString = repeatedString + str
-  }
-  return repeatedString
 }
 
 module.exports = repeatStringNumTimes
