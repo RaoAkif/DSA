e4c3898f36b3d7bb0a38ab74038dde73818c0e1d
oshanedesign <oshane.design@gmail.com>
2022-06-12 18:24:13 -0500

    test 1

--- Microverse/05Fibonacci/sum-all-odd-fibonacci-numbers.js
+++ Microverse/05Fibonacci/sum-all-odd-fibonacci-numbers.js
@@ -1,3 +1,19 @@
 module.exports = function (num) {
   // write your code here
-}
+
+  let fibNums = [1,1];
+  let fibSum = fibNums[fibNums.length-2] + fibNums[fibNums.length-1];
+
+  while(fibSum <= num){
+    fibNums.push(fibSum);
+    fibSum = fibNums[fibNums.length-2] + fibNums[fibNums.length-1]
+  }
+
+  return fibNums.filter(fib => (fib % 2 != 0)).reduce((a, b) => a + b)
+};
+
+///6
+
+//1
+//3
+//5
