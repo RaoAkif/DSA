5196995c9d17969427c5935020bce01a68384e26
raoakif <akifrao@gmail.com>
2022-06-26 03:57:54 +0500

    Palindrome Checker Solution

--- Microverse/04Palindrome/palindrome-checker.js
+++ Microverse/04Palindrome/palindrome-checker.js
@@ -1,3 +1,12 @@
 module.exports = function (str) {
   // write your code here
+  //remove all non numerical characters 
+  // turn to lowercase 
+  // create a newstr that will be reverse of str 
+let string = str.replace(/[^a-z0-9]/gis, '').toLowerCase();
+console.log(string);
+let reverseString = str.replace(/[^a-z0-9]/gis, '').toLowerCase().split('').reverse().join('');
+console.log(reverseString);
+
+return string === reverseString ? true : false;
 }
